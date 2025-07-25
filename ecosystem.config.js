6801fb2c1d963ae877d3569c6b2cd5b03fc00537
William Bruntrager <w.bruntrager@gmail.com>
2025-07-25 17:28:19 +0200

    Log responses

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,7 @@ module.exports = {
       // Memory management settings
       max_memory_restart: '350M', // Restart if memory exceeds 350MB
       kill_timeout: 3000, // Allow 3 seconds for graceful shutdown
+      output: '/home/william/logs/transcription-app.log',
     },
   ],
 }
